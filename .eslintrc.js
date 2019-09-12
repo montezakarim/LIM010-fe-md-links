c09641faf62a9f732acbd86b16c16760bdb0f44c
montezakarim <kmonteza@hotmail.com>
2019-09-12 18:51:03 -0500

    diagrama, creación de carpetas, test y funcion de validatePath

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "plugins": ['jest'],
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {    'linebreak-style': 0,
+    'prefer-destructuring': 0,
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    },
+};
